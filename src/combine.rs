d52554545ffe1f1c13c566591863750822553292
Carter Brainerd <0xCB@protonmail.com>
2022-12-28 12:25:14 -0500

    Add some error checking to reading files

--- src/combine.rs
+++ src/combine.rs
@@ -7,30 +7,45 @@ pub fn combine(wordlists: Vec<String>,
            output: Option<String>,
            delimiter: Option<String>) {
     let mut out_writer = match output {
-        Some(x) => {
-            let path = Path::new(&x);
-            Box::new(File::create(&path).unwrap()) as Box<dyn Write>
-        }
-        None => Box::new(io::stdout()) as Box<dyn Write>,
+      Some(x) => {
+        let path = Path::new(&x);
+        Box::new(File::create(&path).unwrap()) as Box<dyn Write>
+      }
+      None => Box::new(io::stdout()) as Box<dyn Write>,
     };
     // Combine the wordlists
     let mut wordlist = String::new();
     for wordlist_path in wordlists {
-        let mut file = File::open(wordlist_path).unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        wordlist.push_str(&contents);
+      //File::open(wordlist_path).unwrap();
+      let mut file = match File::open(wordlist_path) {
+        Ok(x) => x,
+        Err(e) => {
+          match e.kind() {
+            io::ErrorKind::NotFound => {
+              eprintln!("File not found: {}", e);
+              std::process::exit(1);
+            },
+            _ => {
+              eprintln!("Error: {}", e);
+              std::process::exit(1);
+            }
+          }
+        }
+      };
+      let mut contents = String::new();
+      file.read_to_string(&mut contents).unwrap();
+      wordlist.push_str(&contents);
     }
     // Remove duplicates
     let delim = delimiter.unwrap_or("\n".to_string());
     let mut wordlist: Vec<&str> = wordlist.split(&delim).collect();
     if remove_duplicates {
-        wordlist.sort();
-        wordlist.dedup();
+      wordlist.sort();
+      wordlist.dedup();
     }
     // Sort the wordlist
     if sort {
-        wordlist.sort();
+      wordlist.sort();
     }
     // Write the wordlist to a file or stdout
     out_writer.write(wordlist.join("\n").as_bytes()).unwrap();
